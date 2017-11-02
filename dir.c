5600a08929358394cb824e1b4f9186aa750baf68
judyliu2 <jliu15@stuy.edu>
2017-11-02 18:21:51 -0400

    total size added

--- dir.c
+++ dir.c
@@ -2,25 +2,48 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+
+void list_files(char *);
+
+int file_size(char* ,struct stat*);
 
 void list_files(char * d){
+  int dirf_size = 0;
+  int err;
+  struct stat file;
+  
   struct dirent *entptr;
   DIR* dirx = opendir(d);
   DIR* curr_dir = NULL;
+
   while (entptr  = readdir(dirx)){
-    printf("%s\n", entptr->d_name);
     if(curr_dir = opendir(entptr->d_name)){
-      printf(" is also a directory\n");
-      close(curr_dir);
+      printf("%s", entptr->d_name);
+      printf(" is a directory\n");
+      closedir(curr_dir);
+    }
+    else{
+      err = stat(entptr->d_name, &file);
+      dirf_size += file.st_size;
+      printf("%s", entptr->d_name);
+      printf(" is a regular file\n");
     }
   }
+  
+ 
+  printf("Total Directory Size: %d Bytes\n", dirf_size);
   closedir(dirx);
 
 }
 
-void scan(){
-}
+
+
 
 int main(){
+  printf("Statistics for directory: anything\n");
   list_files("anything");
+  printf("\n");
+  printf("Statistics for currurent directory\n");
+  list_files("./");
 }

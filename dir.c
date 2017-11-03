d5d679fdb257f7e1b49bd26c76d12fcc6c61e1b9
Jeffrey <luojeffrey@yahoo.com>
2017-11-02 20:15:41 -0400

    final changes

--- dir.c
+++ dir.c
@@ -3,47 +3,65 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
 
 void list_files(char *);
-
-int file_size(char* ,struct stat*);
-
-void list_files(char * d){
-  int dirf_size = 0;
-  int err;
-  struct stat file;
-  
-  struct dirent *entptr;
-  DIR* dirx = opendir(d);
-  DIR* curr_dir = NULL;
-
-  while (entptr  = readdir(dirx)){
-    if(curr_dir = opendir(entptr->d_name)){
-      printf("%s", entptr->d_name);
-      printf(" is a directory\n");
-      closedir(curr_dir);
-    }
-    else{
-      err = stat(entptr->d_name, &file);
-      dirf_size += file.st_size;
-      printf("%s", entptr->d_name);
-      printf(" is a regular file\n");
-    }
-  }
-  
- 
-  printf("Total Directory Size: %d Bytes\n", dirf_size);
-  closedir(dirx);
-
-}
-
-
-
+int file_size(char *, struct stat *);
+void print_size(off_t);
 
 int main(){
   printf("Statistics for directory: anything\n");
   list_files("anything");
   printf("\n");
-  printf("Statistics for currurent directory\n");
+  
+  printf("Statistics for current directory\n");
   list_files("./");
+  printf("\n");
+  
+  printf("Testing statistics for invalid directory\n");
+  list_files("test");
+}
+
+void list_files(char *d){  
+  DIR *dirx = NULL, *curr_dir = NULL;
+  off_t dir_size = 0;
+  
+  struct stat file;
+  struct dirent *entry = NULL;
+  
+  if(dirx = opendir(d)){
+    while(entry  = readdir(dirx)){
+      if(curr_dir = opendir(entry->d_name)){	
+	printf("Directory: %s\n", entry->d_name);	
+	closedir(curr_dir);
+      } else {
+	stat(entry->d_name, &file);
+	dir_size += file.st_size;
+	printf("Reg file: %s\n", entry->d_name);	
+      }
+    }
+
+    print_size(dir_size);
+    closedir(dirx);
+  } else {
+    printf("Error: %s\n", strerror(errno));
+  }
+}
+
+void print_size(off_t size){
+  char *units[5] = {"B", "KB", "MB", "GB", "TB"};
+  int count = 0, remainder = 0;
+  
+  while(size / 1000){
+    remainder = size % 1000;
+    size /=  1000;
+    count++;
+  }  
+
+  if((remainder / 100) >= 5)
+    size++;
+
+  printf("Total directory size: %d%s\n", (int) size, units[count]);
 }
--- makefile
+++ makefile
@@ -1,8 +1,11 @@
 app: dir.o
 	@gcc dir.o -o app
-dir.o:
+
+dir.o: dir.c
 	@gcc -c dir.c
+
 run: app
 	@./app
+
 clean:
 	@rm *.o
